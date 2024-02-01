f314096f9b97637dd81ff362d926110f8399926b
Katya <katyanikitina16@gmail.com>
2024-02-01 12:58:08 +0100

    Rendered recipes list title

---
+++ src/app/main/components/recipes-section/recipes-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-section',
+  templateUrl: './recipes-section.component.html',
+  styleUrls: ['./recipes-section.component.scss']
+})
+export class RecipesSectionComponent {
+
+}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,13 +4,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
+import { RecipesSectionComponent } from './components/recipes-section/recipes-section.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     MainPageComponent,
-    HomeComponent
+    HomeComponent,
+    RecipesSectionComponent,
+    RecipesListComponent
   ],
   imports: [
     MainRoutingModule,
