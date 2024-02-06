eb04c59a948d69c795a29f77aff6aabce7981cdc
Katya <katyanikitina16@gmail.com>
2024-02-06 13:53:34 +0100

    Fixed mainComponent  route

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { HeaderComponent } from './components/header/header.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { RecipesSectionComponent } from './components/recipes-section/recipes-section.component';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { RecipeCardComponent } from './components/recipes-list/components/recipe-card/recipe-card.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 
 @NgModule({
