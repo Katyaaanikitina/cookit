832fd2fced15ede87c940a79aecda9a511b6d47d
Katya <katyanikitina16@gmail.com>
2024-02-11 10:48:23 +0100

    Created tooltip

--- src/app/main/components/recipes-section/recipes-list/recipe-card/recipe-card.component.ts
+++ src/app/main/components/recipes-section/recipes-list/recipe-card/recipe-card.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
-
+import { ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Recipe } from 'src/interfaces/recipe';
 
 @Component({
@@ -13,7 +12,10 @@ export class RecipeCardComponent {
 
   titleHeight!: number;
 
+  constructor(private readonly _changeDetectorRef: ChangeDetectorRef) {}
+
   ngAfterViewInit() {
     this.titleHeight = this.title.nativeElement.offsetHeight;
+    this._changeDetectorRef.detectChanges();
   }
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -6,10 +6,9 @@ import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { RecipesSectionComponent } from './components/recipes-section/recipes-section.component';
-import { MainPageComponent } from './main-page.component';
 import { RecipesListComponent } from './components/recipes-section/recipes-list/recipes-list.component';
 import { RecipeCardComponent } from './components/recipes-section/recipes-list/recipe-card/recipe-card.component';
-
+import { MainPageComponent } from './main-page.component';
 
 @NgModule({
   declarations: [
